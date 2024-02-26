0a1472dded22bbd1fca8db8d318d72df22b7a10f
Meadowsys <blazeykirin@gmail.com>
2024-02-26 04:44:04 -0800

    improve some comments and things

--- src/pool.rs
+++ src/pool.rs
@@ -11,39 +11,51 @@ pub trait Pool {
 	/// # Safety
 	///
 	/// The provided slices, when joined together using
-	/// [`SlicesWrap::to_boxed_slice`], must be valid UTF-8
+	/// [`SlicesWrap::to_boxed_slice`], must be valid UTF-8. Strictly speaking
+	/// individual slices don't have to be valid UTF-8, so this should _not_ be
+	/// relied on,but it is very likely that they will be.
 	unsafe fn raw_from_slices(&self, slices: SlicesWrap) -> Self::Raw;
 
 	// --- provided functions ---
 
 	/// note to implementors: The default implementation
-	/// of this function is usually enough.
+	/// of this function is usually enough; however this can be overridden
+	/// for efficiency reasons.
 	unsafe fn raw_from_slice(&self, slice: &[u8]) -> Self::Raw {
 		self.raw_from_slices(SlicesWrap(&[slice]))
 	}
 
 	/// note to implementors: The default implementation
-	/// of this function is usually enough.
+	/// of this function is usually enough; however this can be overridden
+	/// for efficiency reasons.
 	fn raw_from_str(&self, s: &str) -> Self::Raw {
 		unsafe { self.raw_from_slice(s.as_bytes()) }
 	}
 
 	/// note to implementors: The default implementation
-	/// of this function is usually enough.
+	/// of this function is usually enough; however this can be overridden
+	/// for efficiency reasons.
 	fn raw_empty(&self) -> Self::Raw {
 		self.raw_from_str("")
 	}
 
-	/// note to implementors: You probably don't want this (you'll know if you do)
+	/// note to implementors: You probably don't want this (you'll know if you do).
+	/// This can be overridden if you can provide an optimisation by doing so.
 	fn raw_empty_with_capacity(&self, capacity: usize) -> Self::Raw {
+		let _ = capacity;
 		self.raw_empty()
 	}
 }
 
+/// Wraps a slice of slices of bytes. This has a simple hash implementation
+/// that's just done by repeatedly calling `hash` on every u8 in sequence, to
+/// purposefully ensure that by doing the same to just one slice, or any amount of slices,
+/// there will be the same resulting hash as if you were to join the slices.
 #[repr(transparent)]
 pub struct SlicesWrap<'h>(pub &'h [&'h [u8]]);
 
 impl<'h> SlicesWrap<'h> {
+	/// Joins the slices in sequence, returning a boxed slice.
 	pub fn to_boxed_slice(&self) -> Box<[u8]> {
 		self.into_iter()
 			.collect::<Vec<_>>()
@@ -68,13 +80,17 @@ impl<'h> IntoIterator for &SlicesWrap<'h> {
 	}
 }
 
+/// Iterator for [`SlicesWrap`] that returns elements in one slice after the
+/// other in sequence. It would be the same as calling `.zip` with all the slices.
+/// Iterating through the boxed slice returned by [`SlicesWrap::to_boxed_slice`]
+/// would also yield the same sequence as if you were to iterate through this iterator.
 pub struct SlicesWrapIter<'h>(Vec<&'h [u8]>);
 
 impl<'h> Iterator for SlicesWrapIter<'h> {
 	type Item = u8;
 
 	fn next(&mut self) -> Option<u8> {
-		// if this is none
+		// if this is None
 		// we will have reached the end of the vec
 		let next_slice = self.0.pop()?;
 
