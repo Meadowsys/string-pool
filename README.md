d73fb1b2c6f7ec29202aea150f60108b19c41563
Meadowsys <blazeykirin@gmail.com>
2024-02-26 08:29:22 -0800

    Deref among many other inherent fns
    push, as_bytes, truncate, pop, remove, retain

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ Docs for the current published version of the crate can be found [on docs.rs](ht
 ## Known issues
 
 - Cannot implement `as_mut_str`
-- Doesn't make sense to implement `with_capacity`, `capacity` (no meaningful value to give, and makes no sense in the context of string pool)
+- Doesn't make sense to implement capacity related methods (makes no sense in the context of string pool)
+  - `with_capacity`, `capacity`, `reserve`, `reserve_exact`, `try_reserve`, `try_reserve_exact`, `shrink_to_fit`, `shrink_to`
 - Cannot implement `from_raw_parts`, `into_raw_parts`
--- src/string.rs
+++ src/string.rs
@@ -1,4 +1,5 @@
 use crate::pool::{ GlobalPool, Pool, SlicesWrap };
+use ::std::ops::Deref;
 use ::std::string::{ self as std_string, String as StdString };
 use ::std::str as std_str;
 
@@ -127,6 +128,104 @@ impl<P: Pool> String<P> {
 
 	// skipping (nightly, for now): extend_from_within
 	// skipping: capacity
+	// skipping: reserve
+	// skipping: reserve_exact
+	// skipping: try_reserve
+	// skipping: try_reserve_exact
+	// skipping: shrink_to_fit
+	// skipping: shrink_to
+
+	pub fn push(&mut self, ch: char) {
+		self.push_str(ch.encode_utf8(&mut [0u8; 4]));
+	}
+
+	pub fn as_bytes(&self) -> &[u8] {
+		self.pool.raw_to_slice(&self.raw)
+	}
+
+	pub fn truncate(&mut self, new_len: usize) {
+		if new_len > self.len() { return }
+
+		assert!(self.is_char_boundary(new_len));
+		let new_slice = &self.pool.raw_to_slice(&self.raw)[..new_len];
+		let new_raw = unsafe { self.pool.raw_from_slice(new_slice) };
+
+		self.raw = new_raw;
+	}
+
+	pub fn pop(&mut self) -> Option<char> {
+		let ch = self.chars().next_back()?;
+		let new_len = self.len() - ch.len_utf8();
+
+		let new_slice = &self.pool.raw_to_slice(&self.raw)[..new_len];
+		let new_raw = unsafe { self.pool.raw_from_slice(new_slice) };
+
+		self.raw = new_raw;
+		Some(ch)
+	}
+
+	pub fn remove(&mut self, i: usize) -> char {
+		// let slice =
+		let ch = self[i..].chars().next()
+			.expect("cannot remove a char from the end of a string");
+		let next = i + ch.len_utf8();
+
+		let slice = self.pool.raw_to_slice(&self.raw);
+		let new_raw = unsafe {
+			self.pool.raw_from_slices(SlicesWrap(&[
+				&slice[..i],
+				&slice[next..]
+			]))
+		};
+
+		self.raw = new_raw;
+		ch
+	}
+
+	// skipping (nightly, for now): remove_matches
+
+	pub fn retain<F>(&mut self, mut f: F)
+	where
+		F: FnMut(char) -> bool
+	{
+		// reason for capacity:
+		// worst case is true, false, true, false, etc
+		// which is keeping half of 1 byte chars
+		// so if chars are longer or longer sequences of no switching, it'll be less
+		// +1 is for odd ones, like. true false true, len 3, (3 / 2) + 1 == 2, will fit
+		// i mean, worst case is worse performance, it doesn't lead to correctness
+		// issues, so its not the most critical :p
+		let mut retained = Vec::with_capacity((self.len() / 2) + 1);
+		let mut state = None;
+
+		for (i, char) in self.char_indices() {
+			match (f(char), state) {
+				// start new streak
+				(true, None) => {
+					state = Some(i);
+				}
+
+				// end streak
+				(false, Some(i_start)) => {
+					retained.push(self[i_start..i].as_bytes());
+					state = None;
+				}
+
+				// continue true streak
+				(true, Some(_)) => { /* noop */ }
+
+				// continue false streak
+				(false, None) => { /* noop */ }
+			}
+		}
+
+		if let Some(i_start) = state {
+			retained.push(self[i_start..].as_bytes());
+		}
+
+		let new_raw = unsafe { self.pool.raw_from_slices(SlicesWrap(&retained)) };
+		self.raw = new_raw;
+	}
 }
 
 impl From<&str> for String {
@@ -139,5 +238,12 @@ impl<P: Pool> From<(&str, P)> for String<P> {
 	fn from((s, pool): (&str, P)) -> Self {
 		let raw = unsafe { pool.raw_from_slice(s.as_bytes()) };
 		Self { raw, pool }
+	}
+}
+
+impl<P: Pool> Deref for String<P> {
+	type Target = str;
+	fn deref(&self) -> &str {
+		self.as_str()
 	}
 }
